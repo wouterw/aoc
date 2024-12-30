aee6d063cd92b2bb7077561390492b1ebe6c7b89
Wouter Willaert <wouter.willaert@gmail.com>
2024-12-30 10:05:44 +0100

    2024.07 p2

--- 2024/day07/src/main.rs
+++ 2024/day07/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let input = include_str!("input.txt");
 
     println!("Part 1: {:?}", part_one(input));
+    println!("Part 2: {:?}", part_two(input));
 }
 
 fn parse(input: &str) -> Vec<(u64, Vec<u64>)> {
@@ -29,11 +30,10 @@ fn parse(input: &str) -> Vec<(u64, Vec<u64>)> {
 enum Op {
     Add,
     Multiply,
+    Concat,
 }
 
-fn solve_equation(result: u64, numbers: Vec<u64>) -> Option<u64> {
-    let operators = vec![Op::Add, Op::Multiply];
-
+fn solve_equation(result: u64, numbers: Vec<u64>, operators: &Vec<Op>) -> Option<u64> {
     let k = numbers.len() - 1;
     let permutations = itertools::repeat_n(operators, k).multi_cartesian_product();
 
@@ -50,6 +50,7 @@ fn solve_equation(result: u64, numbers: Vec<u64>) -> Option<u64> {
             let result = match op {
                 Op::Add => a + b,
                 Op::Multiply => a * b,
+                Op::Concat => format!("{}{}", a, b).parse().unwrap(),
             };
 
             numbers.insert(0, result);
@@ -65,10 +66,21 @@ fn solve_equation(result: u64, numbers: Vec<u64>) -> Option<u64> {
 
 fn part_one(input: &str) -> u64 {
     let equations = parse(input);
+    let operators = vec![Op::Add, Op::Multiply];
 
     equations
         .iter()
-        .filter_map(|(result, numbers)| solve_equation(*result, numbers.clone()))
+        .filter_map(|(result, numbers)| solve_equation(*result, numbers.clone(), &operators))
+        .sum()
+}
+
+fn part_two(input: &str) -> u64 {
+    let equations = parse(input);
+    let operators = vec![Op::Add, Op::Multiply, Op::Concat];
+
+    equations
+        .iter()
+        .filter_map(|(result, numbers)| solve_equation(*result, numbers.clone(), &operators))
         .sum()
 }
 
@@ -89,5 +101,20 @@ mod tests {
 292: 11 6 16 20"#;
 
         assert_eq!(part_one(input), 3749);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = r#"190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"#;
+
+        assert_eq!(part_two(input), 11387);
     }
 }
